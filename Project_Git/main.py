e2e4358f3c15cf3da3778ee524e2afb17984b0e8
Suraj <kantisuraj716@gmail.com>
2026-01-20 16:27:51 +0530

    Implemented the commit

--- Project_Git/main.py
+++ Project_Git/main.py
@@ -1,7 +1,7 @@
 import argparse
-import sys ,json ,hashlib,zlib
+import sys ,json ,hashlib,zlib,time
 from pathlib import Path
-from typing import Dict
+from typing import Dict,List,Tuple
 
 
 class GitObject:
@@ -25,7 +25,7 @@ class GitObject:
     def deserialize(cls,data:bytes)->"GitObject": 
         decompressed = zlib.decompress(data) 
         null_idx = decompressed.find(b"\0")
-        header = decompressed[:null_idx]
+        header = decompressed[:null_idx].decode()
         content = decompressed[null_idx + 1:]
 
         obj_type , size = header.split(" ") 
@@ -39,6 +39,110 @@ class Blob(GitObject):
     
     def get_content(self)->bytes:
         return self.content
+
+class Tree(GitObject):
+    #Tree object for hierarchy
+    
+    def __init__(self,entries:List[Tuple[str,str,str]]=None):
+        self.entries = entries or []
+        content = self._serialize_entries() #of the entire list contains multiple files folder 
+        super().__init__('tree',content)
+        
+    def _serialize_entries(self)->bytes:
+        #<mode> <name>\0<hash><mode> <name>\0<hash><mode> <name>\0<hash> tree content is like this
+        #why sorted to give (a.txt,b.txt) or we do (b.txt,a.txt) hash reamins same
+        content = b""
+        for mode,name,obj_hash in sorted(self.entries):
+            content += f"{mode} {name}\0".encode()
+            content += bytes.fromhex(obj_hash)#hash is hexa_Decimal so we convert back to bytes
+        return content
+    
+    def add_entry(self,mode:str,name:str,obj_hash:str):
+        self.entries.append((mode,name,obj_hash))
+        self.content = self._serialize_entries() #directly changing the content variable og gitobj class
+
+    @classmethod
+    # because we nedd to apply the logic to entire class not a particular object
+    def from_content(cls,content:bytes)->"Tree": 
+        tree = cls() # empty instance of the class being assigned to the variable name tree
+        i = 0
+
+        while i < len(content):
+            null_idx = content.find(b"\0",i)
+            # 100644 README.md\0[20bytes of content hash]100644 xyz.md\0[20bytes of content hash]
+            if null_idx == -1:
+                break
+            mode_name = content[i:null_idx].decode()
+            mode,name = mode_name.split(' ',1)
+            obj_hash = content[null_idx + 1 : null_idx + 21].hex()
+            tree.entries.append((mode,name,obj_hash))#class method
+
+            i = null_idx + 1
+        return tree
+
+
+class Commit(GitObject):
+    def __init__(
+            self,
+            tree_hash:str,
+            parent_hashes:List[str],
+            author:str,
+            committer:str,
+            message:str,
+            timestamp:int = None,
+        ):
+            self.tree_hash = tree_hash
+            self.parent_hashes = parent_hashes 
+            self.author = author
+            self.committer=committer
+            self.message = message
+            self.timestamp =timestamp or int(time.time())
+
+            content = self._serialize_commit()
+            super().__init__("commit",content)
+        
+    def _serialize_commit(self):
+        lines = [f"tree {self.tree_hash}"]
+        for parent in self.parent_hashes:
+            lines.append(f"parent {parent}")   
+        lines.append(f"author {self.author} {self.timestamp} +0000")
+        lines.append(f"committer {self.committer} {self.timestamp} +0000")
+        lines.append("")
+        lines.append(self.message)
+
+        return "\n".join(lines).encode()
+    
+    @classmethod
+    def from_content(cls,content:bytes)->"Commit":
+        lines = content.decode().split("\n")
+        tree_hash = None
+        parent_hashes = []
+        author = None
+        committer =None
+        message_start = 0
+
+        for i , line in enumerate(lines):
+            if line.startswith("tree "):
+                tree_hash = line[5:]
+            elif line.startswith("parent "):
+                parent_hashes.append(line[7:])
+            elif line.startswith("author "):
+                author_parts = line[7:].rsplit(" ",2)
+                author = author_parts[0]
+                timestamp = int(author_parts[1])
+            elif line.startswith("committer "):
+                commit_parts = line[10:].rsplit(" ",2)
+                committer = commit_parts[0]
+            elif line == "":
+                message_start = i + 1
+                break
+        message = "\n".join(lines[message_start:])
+        commit = cls(tree_hash,parent_hashes,author,committer,message,timestamp)
+
+        return commit
+            
+
+
 
 class Repository:
     #creating the .git folder
@@ -98,6 +202,15 @@ class Repository:
     def save_index(self,index:Dict[str,str]):
         self.index_file.write_text(json.dumps(index,indent=2))
 
+    def load_object(self,obj_hash:str)->GitObject:
+        obj_dir = self.objects_dir / obj_hash[:2]
+        obj_file = obj_dir / obj_hash[2:]
+
+        if not obj_file.exists():
+            raise FileNotFoundError(f"Object {obj_hash} not found")
+        return GitObject.deserialize(obj_file.read_bytes())
+    
+
     def add_file(self,path:str):
         full_path = self.path / path
         if not full_path.exists():
@@ -149,8 +262,6 @@ class Repository:
             print(f"Directory {path} already up to date")
         
 
-        
-
     def add_path(self,path:str)->None:
         full_path = self.path / path
         if not full_path.exists():
@@ -161,8 +272,110 @@ class Repository:
             self.add_directory(path)
         else:
             raise ValueError(f"{path} is neither a file nor a directory")
+
+    def create_tree_from_index(self):
+        index = self.load_index()
+        if not index:
+            tree = Tree()
+            return self.store_object(tree)
+        
+        dirs = {}
+        files = {}
+        for file_path,blob_hash in index.items():
+            parts = file_path.split("/")
+            if len(parts) == 1:
+                #file in root
+                files[parts[0]] = blob_hash
+            else:
+                dir_name = parts[0]
+                if dir_name not in dirs:
+                    dirs[dir_name] = {}
+                
+                current = dirs[dir_name]
+                for part in parts[1:-1]: #last part not included
+                    if part not in current:
+                        current[part] = {}
+
+                    current = current[part]   
+                current[parts[-1]] = blob_hash
+
+        def create_tree_recursively(entries_dict:Dict):
+            tree = Tree()
+
+            for name,blob_hash in entries_dict.items():
+                if isinstance(blob_hash,str):
+                    tree.add_entry("100644",name,blob_hash)
+                
+                if isinstance(blob_hash,dict):
+                    subtree_hash = create_tree_recursively(blob_hash)
+                    tree.add_entry("40000",name,subtree_hash)
+                
+            return self.store_object(tree)
+
+        #decomposing it into one root directory
+        root_entries = {**files}
+        for dir_name,dir_contents in dirs.items():
+            root_entries[dir_name] = dir_contents
+        #do we need to write index to with root_dir no index is always a flat list of paths
+        return create_tree_recursively(root_entries)
+       
+    #current branch is stored in the HEAD file  
+    def get_current_branch(self)->str:
+        if not self.head_file.exists():
+            return "master"
+        head_content = self.head_file.read_text().strip()
+        if head_content.startswith("ref: refs/heads/"):
+            return head_content[16:]
+        return "HEAD" #detached Head
+    
+    #current commit is present in refs/head/filename(poimting to a commit_hash)
+    def get_branch_commit(self,current_branch:str):
+        branch_file = self.heads_dir / current_branch
+
+        if branch_file.exists():
+            return branch_file.read_text().strip()
+        return None
+
+    def set_branch_commit(self,current_branch:str,commit_hash):
+        branch_file = self.heads_dir / current_branch
+        branch_file.write_text(commit_hash + "\n")
         
 
+    def commit(self,message:str,author:str):
+        # create the tree object from the index (staging area)
+        tree_hash = self.create_tree_from_index()
+
+        current_branch = self.get_current_branch()
+        parent_commit = self.get_branch_commit(current_branch)
+        parent_hashes = [parent_commit] if parent_commit else [] #subsequent commit: parent_commit holds the hash of the previous commit. This line creates a list with that one hash inside it: ["abc123..."].
+        
+        index = self.load_index()
+        if not index:
+            print("nothing to commit working tree clean")
+            return None
+        
+        if parent_commit:
+            parent_git_commit_obj = self.load_object(parent_commit)
+            parent_commit_data = Commit.from_content(parent_git_commit_obj.content) 
+            if tree_hash == parent_commit_data.tree_hash:
+                print("nothing to commit,working tree clean")
+                return None
+
+        commit = Commit(
+            tree_hash=tree_hash,
+            parent_hashes = parent_hashes,
+            author=author,
+            committer=author,
+            message=message,
+        )
+        #Save the commit object to the objects database
+        commit_hash = self.store_object(commit)
+        # 5. UPDATE THE BRANCH POINTER
+        self.set_branch_commit(current_branch,commit_hash)
+        self.save_index({})
+        print(f"Created commit {commit_hash} on branch {current_branch}")
+        return commit_hash
+        
 
 def main():
     parser = argparse.ArgumentParser(
@@ -172,9 +385,17 @@ def main():
 
     # init command
     init_parser = subparsers.add_parser("init",help="Initialize a new respository")
+    
     #add command
     add_parser = subparsers.add_parser("add",help="Add files and directories to the staging Area")
     add_parser.add_argument("paths",nargs='+',help="Files and directories to add")
+
+    #commit command
+
+    commit_parser = subparsers.add_parser("commit",help="commit the latest changes")
+    commit_parser.add_argument("-m","--message",help="Create a new commit",required=True)
+    commit_parser.add_argument("--author",help="Author nameand email")
+
 
 
     args = parser.parse_args()
@@ -193,7 +414,14 @@ def main():
                 print("Not a git repository")  
                 return
             for path in args.paths:
-                repo.add_path(path)              
+                repo.add_path(path) 
+        elif args.command == "commit":
+            if not repo.get_dir.exists():
+                print("Not a git repository")  
+                return
+            author = args.author or "PyGit user <user@pygit.com>"
+            repo.commit(args.message,author)
+
 
 
     except Exception as e:
--- Project_Git/working .md
+++ Project_Git/working .md
@@ -34,3 +34,110 @@ Your Mental Model,Actual Git Internal
 """Folder contains another folder""",Close—it's usually Folder -> File. No second nested folder.
 """Contains a file in bytes""","Correct! It's a Zlib-compressed ""Blob"" (Binary Large Object)."
 
+1. The "Content" is the Raw DataIn your code, the content passed to the GitObject.__init__ is always the uncompressed, raw data of that specific object type.
+For a Blob: The content is just the raw bytes of your file (e.g., the text inside hello.txt).For a Tree: The content is the list of files and their hashes, formatted specifically: <mode> <name>\0<binary_hash>.
+None of the objects pass compressed data to GitObject. Compression only happens inside the serialize() method.2. 
+Why the Tree "Content" looks differentA Blob is simple: it's just a bucket of data. But a Tree is a directory listing. 
+To make it "Git-compliant," we have to format that listing into a single byte-string 
+so we can hash it.When you do content += bytes.fromhex(obj_hash), 
+you are converting the 40-character hex string (like 5e1c...) into 20 raw bytes.
+This makes the Tree object very compact.3. 
+Here is the logical flow for both Blobs and Trees:
+
+Prepare Raw Content: * Blob: b"hello world"
+
+Tree: b"100644 file.txt\0<20-byte-hash>"
+
+Calculate Hash: * The GitObject.hash() method takes that raw content, adds a header (tree <size>\0), and runs SHA-1.
+
+Crucial: We hash the uncompressed data. This ensures that even if compression algorithms change in the future, the hash remains the same.
+
+Serialize (Compress):
+
+Only when we call serialize() to save the file to .pygit/objects do we use zlib.compress().
+
+The Hierarchy
+Think of it like this: A Tree is just a "special file" that contains a map of other files.
+
+# Why we need Tree Object as well as commit object ?
+
+We need tree for hierarchy, Tree can point to blob_hash and it can point to other Tree
+how does the member variable list of the tree:
+[[<mode><name><hash>],[<mode><name><hash>],[<mode><name><hash>],[<mode><name><hash>]]
+<mode(str) -> mode(bytes)> <filename(str) - filename(bytes)>\0<hash(hexa_decimal {40 bytes}) -> hash(binary {20 bytes})>
+
+This is what is stored in the the object in i.e passed to super().__init__('tree',content)
+the deserialize or from_content() methos is used to decode what we have encoded and passed as content in serialize method back in the original structure
+# 100644 README.md\0[20bytes of content hash]100644 xyz.md\0[20bytes of content hash]
+
+Why we might need it let me give the flow again We need Tree object to create the 
+tree_hash = self.create_tree_from_index(). Before we commit we create git add. we add all the file in the staging area the index file is built index as 
+
+{
+  "main.py": "a2b3f2405dce714edbb366453831306a33888c67",
+  "structure.md": "1f79fcadcd2bbebda623838302bf29ece1d1484e",
+    "test/test1/xyz.txt = "hash1"
+    "test/test1/test2/.abc.txt = "hash2"
+    test/test3/pqr.txt = "hash3"
+    script/exe.py = "hash4"
+}
+
+here we call the create_tree_from_index() 
+when create tree called first the root_dict is created it remember tree contains both blob and point to another tree
+//running a for loop we create file_dict , dirs_dict and then finally combine them to make root_dict it will look like this
+
+{ 
+    "main.py": "a2b3f2405dce714edbb366453831306a33888c67",
+    "structure.md": "1f79fcadcd2bbebda623838302bf29ece1d1484e",
+    test{
+        test1{
+            xyz.txt : "hash1
+            test2{
+                abc.txt : "hash2
+            }
+        }
+        test3{
+            pqr.txt : "hash3"
+        }
+
+    }
+    script {
+        exe.py : "hash5"
+    }
+}
+
+remember this dictionary is created each time we create a root_dict
+
+now we pass this root_dict to create_tree_recursive(root_dict) it would recursively create the entries list [[<mode> <name><hash>],[<mode> <name>\0<hash>],[<mode> <name>\0<hash>]]
+
+In the commit object we need tree_hash that's why needed the tree object apart from tree_hash we need parent_hash,messaege(it was directly pass in git commit -m "message") and author and commiter in our case both are same , then we create the content string by cleverly manipulating string and call the super().__init__ function . so that we can now perform all sorts of GitObject member functions on it like hashing, compressing etc .
+
+ The Execution Flow --> When creating a commit:You call Commit(tree_hash="abc...", message="First commit", ...)__init__ runs._serialize_commit runs to create the text body.The result is passed to super().__init__ so it can be hashed and stored.
+
+When reading a commit:You read the bytes from .pygit/objects/xx/xxxx...You decompress them.You pass those bytes to Commit.from_content(data).from_content parses the text.from_content finally calls return cls(...) (which is __init__) to give you a nice Python object.
+
+# current branch is stored in the HEAD file get_current_branch(self)->str:
+
+# current commit is present in refs/head/filename(pointing to a commit_hash) get_branch_commit(self,current_branch:str):
+
+The commit function takes the message , author as input prepare the commit object
+
+commit = Commit(
+            tree_hash=tree_hash,
+            parent_hashes = parent_hashes,
+            author=author,
+            committer=author,
+            message=message,
+        )
+# Gets the current branch and  parent_commit
+current_branch = self.get_current_branch()
+parent_commit = self.get_branch_commit(current_branch) 
+
+# Store / Update the Branch Pointer with set_branch_commit(current_branch,commit_hash)
+# return commit_hash which it got when passed commit object was passed to store_obj()
+
+The reason we pass the entire commit object to store_objects instead of just the bytes is that the object "carries" everything it needs to know about itself.
+
+If you passed only the bytes to self.store_objects(bytes), the function wouldn't know if those bytes represent a Blob, a Tree, or a Commit. It would just be a pile of data. By passing the object, you are passing the data and the tools to handle that data.
+
+The store_objects(object)  inherits from GitObject, it has access to self.hash() and self.serialize(). When you pass the object commit, the repository simply asks the object: "Hey, give me your hash and your compressed bytes," and the object complies.
